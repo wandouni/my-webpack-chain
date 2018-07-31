c99cec222919ce4868f5e8afe4b6dae4981bf9b3
wandouni <2539419557@qq.com>
2018-07-31 15:40:47 +0800

    管理资源-加载css,遇到点问题

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Q & A
+使用style-loader css-loader处理.css文件时，config配置正常，依赖安装正常，依然会报 `Module parse failed: Unexpected token (1:0)
+You may need an appropriate loader to handle this file type`
+
+依赖如下：
+```
+"css-loader": "^1.0.0",
+"less-loader": "^4.1.0",
+"style-loader": "^0.21.0",
+```
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+const config = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                ]
+            }
+        ]
+    }
+}
+module.export = config
